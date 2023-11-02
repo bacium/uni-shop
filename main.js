b6b754af4c91e93beed5718c7e195d9e88a2b34b
bacium <bacium_dc@163.com>
2023-11-02 17:52:03 +0800

    fix:请求home页面数据

--- main.js
+++ main.js
@@ -3,6 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 
+// 导入请求插件
+import {$http} from "@escook/request-miniprogram"
+
+
+
+// 挂载到全局对象uni上面
+uni.$http=$http
+
+// 配置跟地址baseurl
+// $http.baseUrl="https://www.uinav.com"
+$http.baseUrl="https://api-hmugo-web.itheima.net"
+
+// 请求拦截器
+$http.beforeRequest= function (option) {
+	uni.showLoading({
+		title:"数据正在加载……"
+	})
+}
+
+
+// 响应拦截器
+$http.afterRequest=function(option) {
+	uni.hideLoading()
+}
+
+
+
+
 Vue.config.productionTip = false
 
 App.mpType = 'app'
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -8,8 +8,19 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods:{
+		 async	getSwiperList() {
+			 debugger;
+				const {data}=await uni.$http.get('/api/public/v1/home/swiperdata')
+				console.log(data);
+				console.log(123);
+			}
 		}
 	}
 </script>
